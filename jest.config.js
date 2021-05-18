9a38a9e22bd620f7cd7d4ba1b6c8fdd48a6bd23d
ppiyush13 <ppiyush13@gmail.com>
2021-05-18 19:10:20 +0530

    jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,11 @@ module.exports = {
     testEnvironment: 'node',
     coverageReporters: ['html', 'text'],
     testMatch: ['**/src/*.test.js'],
+    setupFilesAfterEnv: ['<rootDir>/src/setup-tests-after-env.js'],
     collectCoverageFrom: [
         'src/**/*.{js,ts}',
         '!<rootDir>/node_modules/',
         '!<rootDir>/src/setup-tests.js',
         '!<rootDir>/src/test-utils/*',
     ],
-    setupFilesAfterEnv: ['<rootDir>/src/setup-tests-after-env.js'],
 };
